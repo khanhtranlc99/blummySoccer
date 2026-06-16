3440d52b8754b587f9887674ed5290326159032e
khanh <trankhanhlc99@gmail.com>
2026-06-16 21:54:54 +0700

    1.0.4

--- Assets/_Game/Scripts/UI/pfb_Intro/pfb_Intro.cs
+++ Assets/_Game/Scripts/UI/pfb_Intro/pfb_Intro.cs
@@ -1,11 +1,28 @@
+using DG.Tweening;
+using Sirenix.OdinInspector.Editor.TypeSearch;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityExtensions;
 
 public class pfb_Intro : UIBehavior
 {
     [SerializeField] Animator animator;
     [SerializeField] CanvasGroup canvasGroup;
+    public Button btnNext;
+    public RotateObj rotateObj;
+    public void OnEnable()
+    {
+        rotateObj.rotate = 30;
+        btnNext.gameObject.SetActive(false);
+        btnNext.transform.localScale = Vector3.zero;
+        btnNext.onClick.RemoveAllListeners();
+        btnNext.onClick.AddListener(delegate { GameManager.Instance.Nextlevel(); btnNext.gameObject.SetActive(false); });
+
+
+
+    }
 
     public void PlayIntro()
     {
@@ -17,7 +34,11 @@ public class pfb_Intro : UIBehavior
     }
     private void Nextlevel()
     {
-        GameManager.Instance.Nextlevel();
+        rotateObj.rotate = 5;
+        btnNext.gameObject.SetActive(true);
+        btnNext.transform.DOScale(new Vector3(1,1,1), 0.5f);
+
+
         Debug.LogError("12346Nextlevel");
     //    Winbox.Setup().Show();
     }
